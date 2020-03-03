150695e960a3edf9dab9408201718d2905d2b990
Krenar123 <krenardardhishta205@gmail.com>
2020-03-03 23:09:33 +0100

    Modified files and tested car.rb file

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,34 @@
 require "car/version"
 
 module Car
-  class Error < StandardError; end
-  # Your code goes here...
+  class MyCar
+    attr_accessor :curr_speed, :color
+    attr_reader :year
+    def initialize(model,year,color)
+      @model = model
+      @year = year
+      @color = color
+      @curr_speed = 0
+    end
+
+    def speed_up(speed)
+      # If curr + speed are less or equal then ... else you cant go faster
+      ( @curr_speed + speed ) <= 200 ? @curr_speed += speed : "You're going to die"
+    end
+
+    def shut_off
+      # If curr_speed is not 0 then you cant shut off else shutted off
+      @curr_speed != 0 ? "You should brake and then shut_off" : "Shutted off"
+    end
+
+    def brake(speed)
+      # If curr_speed - speed is less than 0 then curr_speed = 0 else curr_speed -= speed
+      ( @curr_speed - speed ) < 0 ? @curr-speed = 0 : @curr_speed -= speed 
+    end
+
+    def spray_car(color)
+      @color = color
+    end
+    
+  end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,9 +1,63 @@
-RSpec.describe Car do
-  it "has a version number" do
-    expect(Car::VERSION).not_to be nil
-  end
+# TDD first we test and then write code
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Car do
+  # Im gonna create here 
+
+  # Testing class MyCar
+  describe Car::MyCar do
+
+    # I know that i should create this in every 'it' method but..
+    # I created object here so i can use the same ex for all tests
+    car = Car::MyCar.new("Mercedes",2019,"white")
+
+    context "When initializing car object" do
+
+      # This one and to eq(0) we did in class
+      it " should be instance of Car::MyCar" do
+        expect(car).to be_instance_of(Car::MyCar)
+      end
+
+      it " should  give 0 for curr_speed" do
+
+        expect(car.curr_speed).to eq(0)
+      end
+    end
+
+
+    context "When using method speed_up(100)" do
+      
+      it "'s going to return curr_speed+100" do
+        # Using change matcher with objects ...    
+        expect{car.speed_up(100)}.to change(car, :curr_speed).by(100)
+      end
+
+      it "'s going to return true (Its between 0,200)" do
+        car.speed_up(100)
+        expect(car.curr_speed).to be_between(0,200).inclusive
+      end
+
+    end
+
+    context "When using method shut_off" do
+      it " should'nt be curr_speed != 0" do
+        # We should use brake method (200) to work this one
+        # If we delete car.brake it will be red
+        car.brake(200)
+        car.shut_off
+        expect(car.curr_speed).not_to be > (0)
+      end
+    end
+
+    context "When using method spray_car('red')" do
+      it " should give color = 'red'" do
+        car.spray_car('red')
+        expect(car.color).to eq('red')
+      end
+    end
   end
 end
+
+# There are several way to create describe Car::MyCar
+# I can create here with RSpec.describe Car::MyCar do ...
+
+# I can create new file same that we did in class mycar_spec.rb
